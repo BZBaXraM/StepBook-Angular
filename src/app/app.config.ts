85ae8074d783e81e2f9a45334cff1fd7192669ba
Bahram <baxram1997007@gmail.com>
2024-07-27 18:32:31 +0400

    added a member details and more...

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
-import { provideRouter } from "@angular/router";
-import { provideAnimations } from "@angular/platform-browser/animations";
-import { provideToastr } from "ngx-toastr";
+import {ApplicationConfig, provideZoneChangeDetection} from "@angular/core";
+import {provideRouter} from "@angular/router";
+import {provideAnimations} from "@angular/platform-browser/animations";
+import {provideToastr} from "ngx-toastr";
 
-import { routes } from "./app.routes";
-import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
-import { provideHttpClient, withInterceptors } from "@angular/common/http";
-import { errorInterceptor } from "./interceptors/errors.interceptor";
+import {routes} from "./app.routes";
+import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
+import {errorInterceptor} from "./interceptors/errors.interceptor";
+import {jwtInterceptor} from "./interceptors/jwt.interceptor";
 
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideZoneChangeDetection({ eventCoalescing: true }),
-        provideRouter(routes),
-        provideAnimationsAsync(),
-        provideAnimations(),
-        provideToastr({
-            positionClass: "toast-bottom-right",
-        }),
-        provideHttpClient(withInterceptors([errorInterceptor])),
-        provideAnimationsAsync(),
-    ],
+	providers: [
+		provideZoneChangeDetection({eventCoalescing: true}),
+		provideRouter(routes),
+		provideAnimationsAsync(),
+		provideAnimations(),
+		provideToastr({
+			positionClass: "toast-bottom-right",
+		}),
+		provideHttpClient(withInterceptors([errorInterceptor, jwtInterceptor])),
+		provideAnimationsAsync(),
+	],
 };
--- src/app/members/member-detail/member-detail.component.ts
+++ src/app/members/member-detail/member-detail.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {MembersService} from "../../services/members.service";
+import {ActivatedRoute} from "@angular/router";
+import {Member} from "../../models/member.model";
+import {TabsModule} from "ngx-bootstrap/tabs";
 
 @Component({
-  selector: 'app-member-detail',
-  standalone: true,
-  imports: [],
-  templateUrl: './member-detail.component.html',
-  styleUrl: './member-detail.component.css'
+	selector: 'app-member-detail',
+	standalone: true,
+	imports: [TabsModule],
+	templateUrl: './member-detail.component.html',
+	styleUrl: './member-detail.component.css'
 })
-export class MemberDetailComponent {
+export class MemberDetailComponent implements OnInit {
+	private memberService = inject(MembersService);
+	private route = inject(ActivatedRoute);
+	member?: Member;
+
+	ngOnInit(): void {
+		this.getMember();
+	}
+
+	getMember() {
+		const userName = this.route.snapshot.paramMap.get('username');
+		if (!userName) return;
+		this.memberService.getMember(userName).subscribe({
+			next: (member) => (this.member = member),
+			error: (error) => {
+				console.error(error);
+			}
+		});
+	}
 
 }
